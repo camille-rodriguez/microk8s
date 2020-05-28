7460b6b33671f9ac944081627474a8696c41f561
Camille Rodriguez <camille.rodriguez@canonical.com>
2020-05-28 16:49:29 -0500

    install azdata with pip3 in user space instead of apt

--- microk8s-resources/actions/enable.bigdatacluster.sh
+++ microk8s-resources/actions/enable.bigdatacluster.sh
@@ -27,28 +27,35 @@ else
   echo "Sufficient memory found."
 fi
 
-ubuntu_version=$(lsb_release --short --release)
-if [[ ubuntu_version == '16.04' ]] ; then
-  package_url='https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list'
-  echo "OS version confirmed, proceeding."
-elif [[ ubuntu_version == '18.04' ]]; then
-  package_url='https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)'
-  echo "OS version confirmed, proceeding."
-else
-  echo "The ubuntu version detected ($ubuntu_version) is not compatible with BDC. Please install this addon on either Ubuntu 16.04 or 18.04."
-  exit 1
-fi
+#ubuntu_version=$(lsb_release --short --release)
+#if [[ ubuntu_version == '16.04' ]] ; then
+#  package_url='https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list'
+#  echo "OS version confirmed, proceeding."
+#elif [[ ubuntu_version == '18.04' ]]; then
+#  package_url='https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)'
+#  echo "OS version confirmed, proceeding."
+#else
+#  echo "The ubuntu version detected ($ubuntu_version) is not compatible with BDC. Please install this addon on either Ubuntu 16.04 or 18.04."
+#  exit 1
+#fi
+#
+#echo "----------------------------------------"
+#echo "Installing azdata cli"
+#echo "----------------------------------------"
+#run_with_sudo apt-get update
+#run_with_sudo apt-get install gnupg ca-certificates curl wget software-properties-common apt-transport-https lsb-release -y
+#curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+#run_with_sudo add-apt-repository "$(wget -qO- ${package_url})"
+#run_with_sudo apt-get update
+#run_with_sudo apt-get install -y azdata-cli
 
 echo "----------------------------------------"
 echo "Installing azdata cli"
 echo "----------------------------------------"
-run_with_sudo apt-get update
-run_with_sudo apt-get install gnupg ca-certificates curl wget software-properties-common apt-transport-https lsb-release -y
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
-run_with_sudo add-apt-repository "$(wget -qO- ${package_url})"
-run_with_sudo apt-get update
-run_with_sudo apt-get install -y azdata-cli
-
+pip3 install -r https://aka.ms/azdata --user --no-warn-script-location
+azdata_path="${HOME}/.local/bin/azdata"
+echo "----------------------------------------"
+echo "azdata has been successfully installed in ${azdata_path}"
 
 echo "----------------------------------------"
 echo "Enabling dns and storage addons"
@@ -56,20 +63,13 @@ echo "----------------------------------------"
 "$SNAP/microk8s-enable.wrapper" dns storage
 sleep 5
 
-
-sudo snap install kubectl --classic
 #mkdir ~/.kube
 #run_with_sudo microk8s.kubectl config view --raw > .kube/config #or export KUBECONFIG=path/to/.kube/config
 
-#we should deploy from a template already generated here
-#microk8s creates a local storage, let's test with that
-#vi /var/snap/microk8s/current/args/kube-apiserver -> add a line > --allow-privileged and restart microk8s CAN BE AVOIDED WITH FOLLOWING
-# azdata bdc config init --source kubeadm-dev-test --target dev-test --accept-eula='yes'
-# azdata bdc config patch --config-file dev-test/control.json --patch-file storageclass-patch.json
-# azdata bdc config patch --config-file dev-test/control.json --patch-file elasticsearch-patch.json
-
+# azdata is used to deploy a customized config of Big Data Clusters onto microk8s.
+# The config files are located in ${SNAP}/actions/bdc
 echo "----------------------------------------"
 echo "Deploying BDC with azdata"
 echo "----------------------------------------"
-azdata bdc create --config-profile ${config_profile_path} --accept-eula='yes'
+${azdata_path} bdc create --config-profile ${config_profile_path} --accept-eula='yes'
 
